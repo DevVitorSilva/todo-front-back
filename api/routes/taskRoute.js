44d6406fdbc064d8b73563eb8627e8395502ea9e
João Vitor <joao@joaovitor.com>
2023-05-26 09:39:59 -0300

    Criada as rotas: GET, POSt, PUT e DELETE.

--- api/routes/taskRoute.js
+++ api/routes/taskRoute.js
@@ -5,23 +5,27 @@ import { TaskModel } from "../models/TaskModel.js";
 
 const route = Router();
 
-route.get('/api', (req, res) => {
-    res.send('hello tarefas');
+route.get('/api', async (req, res) => {
+    const tasks = await TaskModel.find({});
+    res.json(tasks);
 });
 
-route.post('/api', e.json(), (req, res) => {
+route.post('/api', e.json(),async (req, res) => {
     const {task} = req.body;
-
     const tarefa = new TaskModel({task});
+    await tarefa.save()
+    res.send('200');
+});
 
-    tarefa.save().
-    then((doc) => {
-        console.log(doc);
-    }).
-    catch((err) => {
-        console.log(`Erro ao salvar o documento: ${err}`);
-    });
+route.put('/api', e.json(), async (req, res) => {
+    const {id, task} = req.body;
+    await TaskModel.findOneAndUpdate({_id: id}, {task: task});
+    res.send('200');
+});
 
+route.delete('/api', e.json(), async (req, res) => {
+    const {id} = req.body;
+    await TaskModel.findByIdAndDelete(id);
     res.send('200');
 });
 
