653c3cdac7ff9483cb8aa68084413f138cd46757
João Vitor <joao@joaovitor.com>
2023-05-30 17:06:10 -0300

    foi adicionada rota GET com pareams e criado o arquivo script.js

--- api/routes/taskRoute.js
+++ api/routes/taskRoute.js
@@ -11,26 +11,38 @@ const corsOptins = {
 
 route.use(cors(corsOptins));
 
+route.get('/api/:id', async(req, res) => {
+    try {
+        const {id} = req.params;
+        const task = await TaskModel.findById(id);
+
+        res.send(JSON.stringify(task));
+    } catch (err) {
+        res.send(err);
+    }
+    
+})
+
 route.get('/api', async (req, res) => {
     const tasks = await TaskModel.find({});
     res.json(tasks);
 });
 
-route.post('/api', e.json(),async (req, res) => {
-    const {task} = req.body;
-    const tarefa = new TaskModel({task});
+route.post('/api', e.json(), async (req, res) => {
+    const { task } = req.body;
+    const tarefa = new TaskModel({ task });
     await tarefa.save()
     res.send('200');
 });
 
 route.put('/api', e.json(), async (req, res) => {
-    const {id, task} = req.body;
-    await TaskModel.findOneAndUpdate({_id: id}, {task: task});
+    const { id, task } = req.body;
+    await TaskModel.findOneAndUpdate({ _id: id }, { task: task });
     res.send('200');
 });
 
 route.delete('/api', e.json(), async (req, res) => {
-    const {id} = req.body;
+    const { id } = req.body;
     await TaskModel.findByIdAndDelete(id);
     res.send('200');
 });
--- frontend/script/script.js
+++ frontend/script/script.js
@@ -1,16 +1,82 @@
 "use strict"
 
-function getAll(){
-    fetch('http://localhost:9000/api', {mode: 'cors'}).
-    then((res) => {
-        return res.json()
-    }).
-    then((tasks) => {
-        console.log(tasks)
-    }).
-    catch((err) => {
-        console.log(err)
-    })
+document.addEventListener('DOMContentLoaded', () => {
+    getAll();
+});
+
+function getAll() {
+    fetch('http://localhost:9000/api', { mode: 'cors' })
+        .then((res) => {
+            return res.json()
+        })
+        .then((tasks) => {
+
+            let taskElements = '';
+            tasks.forEach(element => {
+                let taskElement = `
+                    <div>
+                        <input type="checkbox">
+                        <label>${element.task}</label>
+                        <button onclick="getOne(this)" id="${element._id}">Edit</button>
+                        <button>Delete</button>
+                    </div>
+                `
+                taskElements += taskElement;
+            });
+
+            document.querySelector('.allTasks').innerHTML = taskElements;
+
+        })
+        .catch((err) => {
+            console.log(err)
+        });
 }
 
-getAll()
+function addTask() {
+    const taskContent = document.querySelector('#taskContent').value;
+
+    const taskObj = {
+        task: taskContent
+    }
+
+    const options = {
+        method: 'POST',
+        headers: new Headers({ 'content-type': 'application/json' }),
+        body: JSON.stringify(taskObj)
+    }
+
+    fetch('http://localhost:9000/api', options)
+        .then(() => {
+            getAll();
+            document.querySelector('#taskContent').value = '';
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+
+}
+
+async function getOne(element) {
+    const id = element.id;
+    const label = element.parentElement.children[1].textContent;
+    
+    const docTask = await fetch(`http://localhost:9000/api/${id}`)
+        .then((doc) => {
+            return doc.json()
+        })
+        .catch((err) => {
+            console.log(err)
+        });
+
+    console.log(label)
+    console.log(docTask)
+}
+
+// function teste(element) {
+//     const input = document.createElement('input')
+//     const button = document.createElement('input')
+
+
+//     element.appendChild(button)
+//     element.appendChild(input)
+// }
